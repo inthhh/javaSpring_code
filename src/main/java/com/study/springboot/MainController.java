0716188fa8f7f7cf17c0873af78f29ccb82bc296
inthhh <glass005533@gmail.com>
2025-04-03 13:49:58 +0900

    Thymeleaf Model 실습

---
+++ src/main/java/com/study/springboot/MainController.java
@@ -0,0 +1,24 @@
+package com.study.springboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String main(){
+//        return "index.html"; // static에서는 이렇게 쓰지만
+        return "index"; // 타임리프에서는 알아서 타입을 내려줌 (랜더해서 내려줌)
+    }
+    
+    @GetMapping("/index")
+    public String index1(Model model){
+        // Model 클래스 : 스프링에서 데이터를 담아 전달하는 용도의 클래스 객체
+        model.addAttribute("name_text", "<ins>홍길동</ins>");
+        model.addAttribute("name_html", "<ins>홍길동</ins>");
+        System.out.println("name_text = " + model.getAttribute("name_text"));
+        System.out.println("name_html = " + model.getAttribute("name_html"));
+        return "index";
+    }
+}
