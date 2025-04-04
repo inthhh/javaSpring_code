f187f5a851e395ab88591eb65d8833d8068155af
inthhh <glass005533@gmail.com>
2025-04-04 13:49:38 +0900

    API 응답 세팅 (JSON response)

--- src/main/java/com/study/springboot/ApiController.java
+++ src/main/java/com/study/springboot/ApiController.java
@@ -1,7 +1,15 @@
 package com.study.springboot;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // 클래스의 모든 응답이 문자열로 선언됨 (JSON)
+@RequestMapping("/api/v1") // 모든 경로가 ~/api/v1부터 시작함
 public class ApiController {
+    // http://localhost:8080/api/v1/login
+    @RequestMapping("/login")
+    public String login(){
+        return "login 함수";
+    }
 }
