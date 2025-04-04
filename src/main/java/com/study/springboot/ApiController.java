194c67bee34566f5e3f1863dde826877aeb8b2e6
inthhh <glass005533@gmail.com>
2025-04-04 14:33:40 +0900

    로그인 API 제작 (JSON response)

--- src/main/java/com/study/springboot/ApiController.java
+++ src/main/java/com/study/springboot/ApiController.java
@@ -1,6 +1,7 @@
 package com.study.springboot;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1") // 모든 경로가 ~/api/v1부터 시작함
 public class ApiController {
     // http://localhost:8080/api/v1/login
-    @RequestMapping("/login")
-    public String login(){
-        return "login 함수";
+    @RequestMapping("/hello")
+    public String hello(){
+        return "api 서버 시작";
+    }
+    
+    // clinet <-> server
+    //        JSON
+    @PostMapping("/login")
+    public ResDto login(@RequestBody ReqDto reqDto) {
+        System.out.println("username = " + reqDto.getUsername());
+        System.out.println("password = " + reqDto.getPassword());
+        
+        return null;
     }
 }
