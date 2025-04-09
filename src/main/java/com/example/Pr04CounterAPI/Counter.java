ab014bbf407514adddd16a17db6fa128d7ddd9c4
inthhh <glass005533@gmail.com>
2025-04-09 10:59:39 +0900

    springboot - 카운터 REST API ver

---
+++ src/main/java/com/example/Pr04CounterAPI/Counter.java
@@ -0,0 +1,14 @@
+package com.example.Pr04CounterAPI;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Counter {
+    private int count;
+}
---
+++ src/main/java/com/example/Pr04CounterAPI/HtmlController.java
@@ -0,0 +1,19 @@
+package com.example.Pr04CounterAPI;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HtmlController {
+    @Autowired
+    private Counter counter;
+    
+    @GetMapping("/")
+    public String main(Model model){
+        model.addAttribute("count", counter.getCount());
+        return "index";
+    }
+}
