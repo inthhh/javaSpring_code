ab8f2046af8918a1942441d1a854a21f76c5b299
inthhh <glass005533@gmail.com>
2025-04-09 11:15:47 +0900

    docs: 주석 추가(Java 변수 자동 초기화)

--- src/main/java/com/example/Pr04CounterAPI/HtmlController.java
+++ src/main/java/com/example/Pr04CounterAPI/HtmlController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HtmlController {
     @Autowired
     private Counter counter;
+    // 자바 int 타입 필드 : 자동으로 0으로 초기화됨
     
     @GetMapping("/")
     public String main(Model model){
